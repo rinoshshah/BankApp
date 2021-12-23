109d9e7b8dabfa4a4d1e1466dec8db80d9113c10
Rinosh Shah <rinosh.shah@gmail.com>
2021-12-23 11:35:20 +0530

    third

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -11,7 +11,11 @@ export class LoginComponent implements OnInit {
 
   accno="Account Number Please"
 
-  users={
+  acno=""
+
+  pswd=""
+
+  users:any={
     1000:{acno:1000,uname:"ram",password:"1000",balance:5000},
     1001:{acno:1001,uname:"ravi",password:"1001",balance:5000},
     1002:{acno:1002,uname:"john",password:"1002",balance:5000}
@@ -22,13 +26,63 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  acnoChange(){
+  acnoChange(event:any){
+    this.acno=event.target.value
+    // console.log(this.acno);
+    
     
   }
 
+  pswdChange(event:any){
+    this.pswd=event.target.value
+    // console.log(this.pswd);
+    
 
-  login(){
-    alert("login clicked")
+  }
+
+
+  // login(){
+  //   var acno = this.acno
+  //   var password = this.pswd
+  //   let database = this.users
+
+  //   if(acno in database){
+  //     if(password==database[acno]["password"]){
+  //       alert("login successful")
+        
+  //     }
+  //     else{
+  //       alert("incorrect password")
+  //     }
+
+  //   }
+  //   else{
+  //     alert("incorrect account number")
+  //   }
+
+    
+  // }
+
+  login(a:any,p:any){
+    var acno = a.value
+    var password = p.value
+    let database = this.users
+
+    if(acno in database){
+      if(password==database[acno]["password"]){
+        alert("login successful")
+        
+      }
+      else{
+        alert("incorrect password")
+      }
+
+    }
+    else{
+      alert("incorrect account number")
+    }
+
+    
   }
 
 }
