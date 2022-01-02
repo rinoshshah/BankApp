ba6f6552ebb6bab88e2530a80030dd2d92526ce8
Rinosh Shah <rinosh.shah@gmail.com>
2022-01-02 22:59:21 +0530

    2022

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { DataService } from '../services/data.service';
 
@@ -17,9 +18,15 @@ export class LoginComponent implements OnInit {
 
   pswd = ""
 
-  
 
-  constructor(private router: Router, private ds: DataService) { }
+  loginForm = this.fb.group({
+    acno: ['', [Validators.required, Validators.pattern('[0-9]*')]],
+    pswd: ['', [Validators.required, Validators.pattern('[a-zA-Z0-9]*')]]
+  })
+
+
+
+  constructor(private router: Router, private ds: DataService, private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
@@ -40,15 +47,21 @@ export class LoginComponent implements OnInit {
 
 
   login() {
-    var acno = this.acno
-    var password = this.pswd
-    // let database = this.ds.users
+    if (this.loginForm.valid) {
+      var acno = this.loginForm.value.acno
+      var password = this.loginForm.value.pswd
+      // let database = this.ds.users
 
-    let result= this.ds.login(acno,password)
+      let result = this.ds.login(acno, password)
 
-    if(result){
-      alert("login successful")
-      this.router.navigateByUrl('dashboard')
+      if (result) {
+        alert("login successful")
+        this.router.navigateByUrl('dashboard')
+      }
+
+    }
+    else {
+      alert("Invalid Form!!!")
     }
 
 
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { DataService } from '../services/data.service';
 
@@ -14,38 +14,46 @@ export class RegisterComponent implements OnInit {
   pswd = ""
   acno = ""
 
-  registerForm=this.fb.group({
-    uname :[''],
-    pswd :[''],
-    acno :['']
+  registerForm = this.fb.group({
+    uname: ['', [Validators.required, Validators.pattern('[a-zA-Z ]*')]],
+    pswd: ['',[Validators.required,Validators.pattern('[a-zA-Z0-9]*')]],
+    acno: ['',[Validators.required,Validators.pattern('[0-9]*')]]
 
   })
 
-  constructor(private ds: DataService, private router: Router,private fb:FormBuilder) { }
+  constructor(private ds: DataService, private router: Router, private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
 
   register() {
 
-    
+
     console.log(this.registerForm);
-    
-    var uname = this.registerForm.value.uname
-    var acno = this.registerForm.value.acno
-    var pswd = this.registerForm.value.pswd
 
-    let result = this.ds.register(acno, pswd, uname)
+    if (this.registerForm.valid) {
+      var uname = this.registerForm.value.uname
+      var acno = this.registerForm.value.acno
+      var pswd = this.registerForm.value.pswd
 
-    if (result == true) {
-      alert("Account registered successfully...")
-      this.router.navigateByUrl("")
+      let result = this.ds.register(acno, pswd, uname)
+
+      if (result == true) {
+        alert("Account registered successfully...")
+        this.router.navigateByUrl("")
+      }
+      else {
+        alert("Account Already Exists !")
+
+      }
     }
     else {
-      alert("Account Already Exists !")
-
+      alert("Invalid form!!!")
     }
+
 
   }
 
+
 }
+
