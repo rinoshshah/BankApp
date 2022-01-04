8cfa78ad8d9a666bbb1c1e0dae318e3c509dfe44
Rinosh Shah <rinosh.shah@gmail.com>
2022-01-04 12:07:58 +0530

    updated getDetails()

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -21,20 +21,20 @@ export class DashboardComponent implements OnInit {
 
 
   depositForm = this.fb.group({
-    acno: ['',[Validators.required,Validators.pattern('[0-9]*')]],
-    pswd: ['',[Validators.required,Validators.pattern('[a-zA-Z0-9]*')]],
-    amount: ['',[Validators.required,Validators.pattern('[0-9]*')]]
+    acno: ['', [Validators.required, Validators.pattern('[0-9]*')]],
+    pswd: ['', [Validators.required, Validators.pattern('[a-zA-Z0-9]*')]],
+    amount: ['', [Validators.required, Validators.pattern('[0-9]*')]]
 
   })
 
   withdrawForm = this.fb.group({
-    acno1: ['',[Validators.required,Validators.pattern('[0-9]*')]],
-    pswd1: ['',[Validators.required,Validators.pattern('[a-zA-Z0-9]*')]],
-    amount1: ['',[Validators.required,Validators.pattern('[0-9]*')]]
+    acno: ['', [Validators.required, Validators.pattern('[0-9]*')]],
+    pswd: ['', [Validators.required, Validators.pattern('[a-zA-Z0-9]*')]],
+    amount: ['', [Validators.required, Validators.pattern('[0-9]*')]]
 
   })
 
-  user=this.ds.currentUserName
+  user = this.ds.currentUserName
 
   constructor(private ds: DataService, private router: Router, private fb: FormBuilder) { }
 
@@ -56,9 +56,9 @@ export class DashboardComponent implements OnInit {
   }
 
   withdraw() {
-    var acno = this.depositForm.value.acno
-    var pswd = this.depositForm.value.pswd
-    var amount = this.depositForm.value.amount
+    var acno = this.withdrawForm.value.acno
+    var pswd = this.withdrawForm.value.pswd
+    var amount = this.withdrawForm.value.amount
 
     let result = this.ds.withdraw(acno, pswd, amount)
 
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -6,7 +6,7 @@ import { Injectable } from '@angular/core';
 export class DataService {
 
 
-  currentUserName:any
+  currentUserName: any
 
   users: any = {
     1000: { acno: 1000, uname: "Ram", password: "1000", balance: 5000 },
@@ -14,17 +14,31 @@ export class DataService {
     1002: { acno: 1002, uname: "John", password: "1002", balance: 5000 }
   }
 
-  constructor() { }
+  constructor() {
+    this.getDetails()
+  }
 
   //to store in localstorage
-  saveDetails(){
+  saveDetails() {
     if (this.users) {
-      localStorage.setItem("userDB",JSON.stringify(this.users))
-      
+      localStorage.setItem("userDB", JSON.stringify(this.users))
+
     }
     if (this.currentUserName) {
-      localStorage.setItem("cUserName",JSON.stringify(this.currentUserName))
-      
+      localStorage.setItem("cUserName", JSON.stringify(this.currentUserName))
+
+    }
+  }
+
+
+  getDetails() {
+    if (localStorage.getItem("userDB")) {
+      this.users = JSON.parse(localStorage.getItem("userDB") || '')
+    }
+
+    if (localStorage.getItem("cUserName")) {
+      this.currentUserName = JSON.parse(localStorage.getItem("cUserName") || '')
+
     }
   }
 
@@ -55,7 +69,7 @@ export class DataService {
     if (acno in database) {
       if (password == database[acno]["password"]) {
 
-        this.currentUserName=database[acno]["uname"]
+        this.currentUserName = database[acno]["uname"]
 
         this.saveDetails()
         return true
@@ -109,17 +123,17 @@ export class DataService {
     var amount = parseInt(amt)
 
     let db = this.users
-    
+
     if (acno in db) {
       if (password == db[acno]["password"]) {
-        if (db[acno]["balance"]>=amount) {
+        if (db[acno]["balance"] >= amount) {
           db[acno]["balance"] = db[acno]["balance"] - amount
 
           this.saveDetails()
-        return db[acno]["balance"]
-          
+          return db[acno]["balance"]
+
         }
-        
+
 
 
       }
